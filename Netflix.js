17766edc9bf41b8eec57e97915414bee4f7c76df
love499520 <52975224+love499520@users.noreply.github.com>
2021-09-29 03:29:31 +0800

    Update Netflix.js

--- Netflix.js
+++ Netflix.js
@@ -17,7 +17,7 @@ const AREA_TEST_FILM_ID = 80018499
       }
 
       result['style'] = 'good'
-      result['content'] = '完整解锁 🎬 𝑵𝒆𝒕𝒇𝒍𝒊𝒙，解锁区域：' + code.toUpperCase()
+      result['content'] = '完整解锁 🎬 𝑵𝒆𝒕𝒇𝒍𝒊𝒙 𝑷𝒓𝒆𝒎𝒊𝒖𝒎，解锁区域：' + code.toUpperCase()
       return Promise.reject('BreakSignal')
     })
     .then((code) => {
@@ -26,13 +26,13 @@ const AREA_TEST_FILM_ID = 80018499
       }
 
       result['style'] = 'info'
-      result['content'] = '仅支持解锁 🎬 𝑵𝒆𝒕𝒇𝒍𝒊𝒙 自制剧，解锁区域：' + code.toUpperCase()
+      result['content'] = '仅支持解锁 🎬 𝑵𝒆𝒕𝒇𝒍𝒊𝒙 𝑷𝒓𝒆𝒎𝒊𝒖𝒎 自制剧，解锁区域：' + code.toUpperCase()
       return Promise.reject('BreakSignal')
     })
     .catch((error) => {
       if (error === 'Not Available') {
         result['style'] = 'alert'
-        result['content'] = '不支持解锁 🎬 𝑵𝒆𝒕𝒇𝒍𝒊𝒙'
+        result['content'] = '不支持解锁 🎬 𝑵𝒆𝒕𝒇𝒍𝒊𝒙 𝑷𝒓𝒆𝒎𝒊𝒖𝒎'
         return
       }
     })
