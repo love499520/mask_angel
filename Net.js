cad483138e91a9ac55de42775b31c7f9b4da1af6
love499520 <52975224+love499520@users.noreply.github.com>
2021-10-01 21:13:51 +0800

    Update Net.js

--- Net.js
+++ Net.js
@@ -8,24 +8,24 @@ const { wifi, v4, v6 } = $network;
 
 let carrierName = '';
 const carrierNames = {
-  //台灣電信業者 MNC Code
-  '466-11': '中華電信','466-92': '中華電信',
-  '466-01': '遠傳電信','466-03': '遠傳電信',
-  '466-97': '台灣大哥大',
-  '466-89': '台灣之星',
-  '466-05': '亞太電信',
-  //中國電信業者 MNC Code
-  '460-03': '中國電信','460-05': '中國電信','460-11': '中國電信',
-  '460-01': '中國聯通','460-06': '中國聯通','460-09': '中國聯通',
-  '460-00': '中國移動','460-02': '中國移動','460-04': '中國移動','460-07': '中國移動','460-08': '中國移動',
-  '460-15': '中國廣電',
-  '460-20': '中國鐵路',
+  //台湾电信运营商 MNC Code
+  '466-11': '中华电信','466-92': '中华电信',
+  '466-01': '远传电信','466-03': '远传电信',
+  '466-97': '台湾大哥大',
+  '466-89': '台湾之星',
+  '466-05': '亚太电信',
+  //中国电信运营商 MNC Code
+  '460-03': '中国电信','460-05': '中国电信','460-11': '中国电信',
+  '460-01': '中国联通','460-06': '中国联通','460-09': '中国联通',
+  '460-00': '中国移动','460-02': '中国移动','460-04': '中国移动','460-07': '中国移动','460-08': '中国移动',
+  '460-15': '中国广电',
+  '460-20': '中国铁通',
 };
 
 if (!v4.primaryAddress && !v6.primaryAddress) {
   $done({
-    title: '沒有網路',
-    content: '尚未連接網際網路\n請檢查網際網路狀態後重試',
+    title: '没有网络',
+    content: '网络未连接\n请检查网络',
     icon: 'wifi.exclamationmark',
     'icon-color': '#CB1B45',
   });
@@ -40,8 +40,8 @@ if (!v4.primaryAddress && !v6.primaryAddress) {
   $httpClient.get('http://ip-api.com/json', function (error, response, data) {
     if (error) {
       $done({
-        title: '發生錯誤',
-        content: '無法獲得目前網路資訊\n請檢查網際網路狀態後重試',
+        title: '发生错误',
+        content: '无法连接到网络\n请检查网络状态',
         icon: 'wifi.exclamationmark',
         'icon-color': '#CB1B45',
       });
@@ -49,7 +49,7 @@ if (!v4.primaryAddress && !v6.primaryAddress) {
     
     const info = JSON.parse(data);
     $done({
-      title: wifi.ssid ? wifi.ssid : '行動數據' + carrierName,
+      title: wifi.ssid ? wifi.ssid : '蜂窝数据' + carrierName,
       content:
         (v4.primaryAddress ? `IPv4 : ${v4.primaryAddress} \n` : '') +
         (v6.primaryAddress ? `IPv6 : ${v6.primaryAddress}\n` : '') +
@@ -59,9 +59,9 @@ if (!v4.primaryAddress && !v6.primaryAddress) {
         (v6.primaryRouter && wifi.ssid
           ? `Router IPv6 : ${v6.primaryRouter}\n`
           : '') +
-        `節點 IP : ${info.query}\n` +
-        `節點 ISP : ${info.isp}\n` +
-        `節點位置 : ${getFlagEmoji(info.countryCode)} | ${info.country} - ${
+        `节点 IP : ${info.query}\n` +
+        `节点 ISP : ${info.isp}\n` +
+        `节点位置 : ${getFlagEmoji(info.countryCode)} | ${info.country} - ${
           info.city
         }`,
       icon: wifi.ssid ? 'wifi' : 'simcard',
