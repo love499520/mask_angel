36043289810eb6d929473a7c0ce8681f4334d3a0
love499520 <52975224+love499520@users.noreply.github.com>
2021-10-01 17:36:12 +0800

    Update Netflix.js

--- Netflix.js
+++ Netflix.js
@@ -1,58 +1,129 @@
-const BASE_URL = 'https://www.netflix.com/title/'
+/**
+ * https://gist.githubusercontent.com/Hyseen/b06e911a41036ebc36acf04ddebe7b9a/raw/nf_check.js
+ * [Panel]
+ * nf_check = script-name=nf_check, title="Netflix 解锁检测", content="请刷新", update-interval=1
+ *
+ * [Script]
+ * nf_check = type=generic, script-path=https://gist.githubusercontent.com/Hyseen/b06e911a41036ebc36acf04ddebe7b9a/raw/nf_check.js?version=1633074636264, argument=title=Netflix 解锁检测
+ *
+ * 支持使用脚本使用 argument 参数自定义配置，如：argument=key1=URLEncode(value1)&key2=URLEncode(value2)，具体参数如下所示，
+ * title: 面板标题
+ * fullContent: 完整解锁时展示的的文本内容，支持两个区域占位符 #REGION_FLAG# 和 #REGION_CODE#，用来展示解锁区域国旗 emoji 和解锁区域编码
+ * fullIcon: 完整解锁时展示的图标，内容为任意有效的 SF Symbol Name
+ * fullIconColor：完整解锁时展示的图标颜色，内容为颜色的 HEX 编码
+ * fullStyle: 完整解锁时展示的图标样式，参数可选值有 good, info, alert, error
+ * onlyOriginalContent：仅解锁自制剧时展示的文本内容，支持两个区域占位符 #REGION_FLAG# 和 #REGION_CODE#，用来展示解锁区域国旗 emoji 和解锁区域编码
+ * onlyOriginalIcon: 仅解锁自制剧时展示的图标
+ * onlyOriginalIconColor: 仅解锁自制剧时展示的图标颜色
+ * onlyOriginalStyle: 仅解锁自制剧时展示的图标样式
+ * notAvailableContent: 不支持解锁时展示的文本内容
+ * notAvailableIcon: 不支持解锁时展示的图标
+ * notAvailableIconColor: 不支持解锁时展示的图标颜色
+ * notAvailableStyle: 不支持解锁时展示的图标样式
+ * errorContent: 检测异常时展示的文本内容
+ * errorIcon: 检测异常时展示的图标
+ * errorIconColor: 检测异常时展示的图标颜色
+ * errorStyle: 检测异常时展示的图标样式
+ */
 
+const BASE_URL = 'https://www.netflix.com/title/'
 const FILM_ID = 81215567
 const AREA_TEST_FILM_ID = 80018499
+const DEFAULT_OPTIONS = {
+  title: '🎬 𝑵𝒆𝒕𝒇𝒍𝒊𝒙 𝑷𝒓𝒆𝒎𝒊𝒖𝒎 解锁检测',
+  fullContent: '完整解锁 🎬 𝑵𝒆𝒕𝒇𝒍𝒊𝒙 𝑷𝒓𝒆𝒎𝒊𝒖𝒎，地区：#REGION_FLAG##REGION_CODE#',
+  fullIcon: '',
+  fullIconColor: '',
+  fullStyle: 'good',
+  onlyOriginalContent: '仅支持解锁 🎬 𝑵𝒆𝒕𝒇𝒍𝒊𝒙 𝑷𝒓𝒆𝒎𝒊𝒖𝒎，地区：#REGION_FLAG##REGION_CODE#',
+  onlyOriginalIcon: '',
+  onlyOriginalIconColor: '',
+  onlyOriginalStyle: 'info',
+  notAvailableContent: '不支持解锁 🎬 𝑵𝒆𝒕𝒇𝒍𝒊𝒙 𝑷𝒓𝒆𝒎𝒊𝒖𝒎',
+  notAvailableIcon: '',
+  notAvailableIconColor: '',
+  notAvailableStyle: 'alert',
+  errorContent: '❌ 检测失败，请重试',
+  errorIcon: '',
+  errorIconColor: '',
+  errorStyle: 'error',
+}
+
+let options = getOptions()
+let panel = {
+  title: options.title,
+}
 
 ;(async () => {
-  let result = {
-    title: '🎬 𝑵𝒆𝒕𝒇𝒍𝒊𝒙 𝑷𝒓𝒆𝒎𝒊𝒖𝒎 解锁检测',
-    style: 'error',
-    content: '❌ 检测失败，请刷新',
-  }
-
   await test(FILM_ID)
-    .then((code) => {
-      if (code === 'Not Found') {
-        return test(AREA_TEST_FILM_ID)
+    .then(async region => {
+      if (options.fullIcon) {
+        panel['icon'] = options.fullIcon
+        panel['icon-color'] = options.fullIconColor ? options.fullIconColor : undefined
+      } else {
+        panel['style'] = options.fullStyle
+      }
+      panel['content'] = options.fullContent.replaceAll('#REGION_FLAG#', getCountryFlagEmoji(region)).replaceAll('#REGION_CODE#', region)
+    })
+    .catch(async error => {
+      if (error !== 'Not Found') {
+        return Promise.reject(error)
       }
 
-      result['style'] = 'good'
-      result['content'] = '完整解锁 🎬 𝑵𝒆𝒕𝒇𝒍𝒊𝒙 𝑷𝒓𝒆𝒎𝒊𝒖𝒎，解锁区域：' + code.toUpperCase()
-      return Promise.reject('BreakSignal')
-    })
-    .then((code) => {
-      if (code === 'Not Found') {
-        return Promise.reject('Not Available')
+      if (options.onlyOriginalIcon) {
+        panel['icon'] = options.onlyOriginalIcon
+        panel['icon-color'] = options.onlyOriginalIconColor ? options.onlyOriginalIconColor : undefined
+      } else {
+        panel['style'] = options.onlyOriginalStyle
       }
 
-      result['style'] = 'info'
-      result['content'] = '仅支持解锁 🎬 𝑵𝒆𝒕𝒇𝒍𝒊𝒙 𝑷𝒓𝒆𝒎𝒊𝒖𝒎 自制剧，解锁区域：' + code.toUpperCase()
-      return Promise.reject('BreakSignal')
-    })
-    .catch((error) => {
-      if (error === 'Not Available') {
-        result['style'] = 'alert'
-        result['content'] = '不支持解锁 🎬 𝑵𝒆𝒕𝒇𝒍𝒊𝒙 𝑷𝒓𝒆𝒎𝒊𝒖𝒎'
+      if (options.onlyOriginalContent.indexOf('#REGION_FLAG#') === -1 && options.onlyOriginalContent.indexOf('#REGION_CODE#') === -1) {
+        panel['content'] = options.onlyOriginalContent
         return
       }
+
+      let region = await test(AREA_TEST_FILM_ID)
+      panel['content'] = options.onlyOriginalContent.replaceAll('#REGION_FLAG#', getCountryFlagEmoji(region)).replaceAll('#REGION_CODE#', region)
     })
-    .finally(() => {
-      $done(result)
+    .catch(error => {
+      if (error !== 'Not Available') {
+        return Promise.reject(error)
+      }
+
+      panel['content'] = options.notAvailableContent
+      if (options.notAvailableIcon) {
+        panel['icon'] = options.notAvailableIcon
+        panel['icon-color'] = options.notAvailableIconColor ? options.notAvailableIconColor : undefined
+      } else {
+        panel['style'] = options.notAvailableStyle
+      }
     })
 })()
+  .catch(error => {
+    console.log(error)
+    if (options.errorIcon) {
+      panel['icon'] = options.errorIcon
+      panel['icon-color'] = options.errorIconColor ? options.errorIconColor : undefined
+    } else {
+      panel['style'] = options.errorStyle
+    }
+    panel['content'] = options.errorContent
+  })
+  .finally(() => {
+    $done(panel)
+  })
 
 function test(filmId) {
   return new Promise((resolve, reject) => {
     let option = {
       url: BASE_URL + filmId,
       headers: {
-        'User-Agent':
-          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36',
       },
     }
     $httpClient.get(option, function (error, response, data) {
       if (error != null) {
-        reject('Error')
+        reject(error)
         return
       }
 
@@ -62,7 +133,7 @@ function test(filmId) {
       }
 
       if (response.status === 404) {
-        resolve('Not Found')
+        reject('Not Found')
         return
       }
 
@@ -73,11 +144,41 @@ function test(filmId) {
         if (region == 'title') {
           region = 'us'
         }
-        resolve(region)
+        resolve(region.toUpperCase())
         return
       }
 
       reject('Error')
     })
   })
+}
+
+function getCountryFlagEmoji(countryCode) {
+  if (countryCode.toUpperCase() == 'TW') {
+    countryCode = 'CN'
+  }
+  const codePoints = countryCode
+    .toUpperCase()
+    .split('')
+    .map(char => 127397 + char.charCodeAt())
+  return String.fromCodePoint(...codePoints)
+}
+
+function getOptions() {
+  let options = Object.assign({}, DEFAULT_OPTIONS)
+  if (typeof $argument != 'undefined') {
+    try {
+      let params = Object.fromEntries(
+        $argument
+          .split('&')
+          .map(item => item.split('='))
+          .map(([k, v]) => [k, decodeURIComponent(v)])
+      )
+      Object.assign(options, params)
+    } catch (error) {
+      console.error(`$argument 解析失败，$argument: + ${argument}`)
+    }
+  }
+
+  return options
 }
