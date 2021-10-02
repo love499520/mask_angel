ba25b6170ac4a73de840b70ea9503097bc21084d
love499520 <52975224+love499520@users.noreply.github.com>
2021-10-02 18:40:56 +0800

    Update Net1.js

--- Net1.js
+++ Net1.js
@@ -66,7 +66,7 @@ $httpClient.get("http://ipwhois.app/json/", function(error, response, data){
         }else if(中国铁通.includes(carrier)){
             运营商 = "China Tietong";
         }else{
-            运营商 = "𝓜𝓸𝓫𝓲𝓵𝓮 𝓝𝓮𝓽𝔀𝓸𝓻𝓴";
+            运营商 = "网络信息";
         }
         if(ip === query){
             $done({
