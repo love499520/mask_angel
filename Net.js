cff1f07d8e2a5eb207fb41b7c8e783580534876b
love499520 <52975224+love499520@users.noreply.github.com>
2021-10-02 22:45:45 +0800

    Update Net.js

--- Net.js
+++ Net.js
@@ -1,25 +1,41 @@
 /**
  * https://raw.githubusercontent.com/Nebulosa-Cat/Surge/main/Panel/Network-Info/test-version/networkCheck_test.js
- * Surge 網路詳情面板
+  * Surge 網路詳情面板
  * @Nebulosa-Cat
  * 詳情請見 README
  */
 const { wifi, v4, v6 } = $network;
 
-let carrierName = '';
+let cellularInfo = '';
 const carrierNames = {
-  //台湾电信运营商 MNC Code
-  '466-11': '中华电信','466-92': '中华电信',
-  '466-01': '远传电信','466-03': '远传电信',
+  //台灣電信業者 MNC Code
+  '466-11': '中华电信', '466-92': '中华电信',
+  '466-01': '远传电信', '466-03': '远传电信',
   '466-97': '台湾大哥大',
   '466-89': '台湾之星',
   '466-05': '亚太电信',
-  //中国电信运营商 MNC Code
-  '460-03': '中国电信','460-05': '中国电信','460-11': '中国电信',
-  '460-01': '中国联通','460-06': '中国联通','460-09': '中国联通',
-  '460-00': '中国移动','460-02': '中国移动','460-04': '中国移动','460-07': '中国移动','460-08': '中国移动',
+  //中國電信業者 MNC Code
+  '460-03': '中国电信', '460-05': '中国电信', '460-11': '中国电信',
+  '460-01': '中国联通', '460-06': '中国联通', '460-09': '中国联通',
+  '460-00': '中国移动', '460-02': '中国移动', '460-04': '中国移动', '460-07': '中国移动', '460-08': '中国移动',
   '460-15': '中国广电',
   '460-20': '中国铁通',
+};
+
+const radioGeneration = {
+  'GPRS': '2.5G',
+  'CDMA1x': '2.5G',
+  'EDGE': '2.75G',
+  'WCDMA': '3G',
+  'HSDPA': '3.5G',
+  'CDMAEVDORev0': '3.5G',
+  'CDMAEVDORevA': '3.5G',
+  'CDMAEVDORevB': '3.75G',
+  'HSUPA': '3.75G',
+  'eHRPD': '3.9G',
+  'LTE': '4G',
+  'NRNSA': '5G',
+  'NR': '5G',
 };
 
 if (!v4.primaryAddress && !v6.primaryAddress) {
@@ -34,7 +50,9 @@ if (!v4.primaryAddress && !v6.primaryAddress) {
     const carrierId = $network['cellular-data'].carrier;
     const radio = $network['cellular-data'].radio;
     if (carrierId && radio) {
-      carrierName = carrierNames[carrierId] ? ' - ' + carrierNames[carrierId] + ' ' + radio : ' - ' + radio;
+      cellularInfo = carrierNames[carrierId] ?
+        carrierNames[carrierId] + ' - ' + radioGeneration[radio] + ' (' + radio + ')' :
+        '蜂窝数据 - ' + radioGeneration[radio] + ' (' + radio + ')';
     }
   }
   $httpClient.get('http://ip-api.com/json', function (error, response, data) {
@@ -46,23 +64,18 @@ if (!v4.primaryAddress && !v6.primaryAddress) {
         'icon-color': '#CB1B45',
       });
     }
-    
+
     const info = JSON.parse(data);
     $done({
-      title: wifi.ssid ? wifi.ssid : '蜂窝数据' + carrierName,
+      title: wifi.ssid ? wifi.ssid : cellularInfo,
       content:
         (v4.primaryAddress ? `IPv4 : ${v4.primaryAddress} \n` : '') +
         (v6.primaryAddress ? `IPv6 : ${v6.primaryAddress}\n` : '') +
-        (v4.primaryRouter && wifi.ssid
-          ? `Router IPv4 : ${v4.primaryRouter}\n`
-          : '') +
-        (v6.primaryRouter && wifi.ssid
-          ? `Router IPv6 : ${v6.primaryRouter}\n`
-          : '') +
+        (v4.primaryRouter && wifi.ssid ? `Router IPv4 : ${v4.primaryRouter}\n` : '') +
+        (v6.primaryRouter && wifi.ssid ? `Router IPv6 : ${v6.primaryRouter}\n` : '') +
         `节点 IP : ${info.query}\n` +
         `节点 ISP : ${info.isp}\n` +
-        `节点位置 : ${getFlagEmoji(info.countryCode)} | ${info.country} - ${
-          info.city
+        `节点位置 : ${getFlagEmoji(info.countryCode)} | ${info.country} - ${info.city
         }`,
       icon: wifi.ssid ? 'wifi' : 'simcard',
       'icon-color': wifi.ssid ? '#005CAF' : '#F9BF45',
