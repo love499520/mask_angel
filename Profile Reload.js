bf93f4fdc7ada19ca20d300cdc28a1146810a950
love499520 <52975224+love499520@users.noreply.github.com>
2022-06-16 21:19:24 +0800

    Delete Profile Reload.js

--- Profile Reload.js
+++
@@ -1,9 +0,0 @@
-$httpAPI("POST", "/v1/profiles/reload", {}, data => {
-    $notification.post("配置刷新","成功","")
-    $done({
-        title: "𝑷𝒓𝒐𝒇𝒊𝒍𝒆 𝑹𝒆𝒍𝒐𝒂𝒅",
-        content: "配置刷新成功",
-        icon: "arrow.triangle.2.circlepath.doc.on.clipboard",
-        "icon-color": "#8B81C3",
-     })
-    });
