4a3b2dc2e981bea2886c00e4a220ef48df0a0299
love499520 <52975224+love499520@users.noreply.github.com>
2022-06-16 21:19:00 +0800

    Update Reload.js

--- Reload.js
+++ Reload.js
@@ -1,7 +1,7 @@
 $httpAPI("POST", "/v1/profiles/reload", {}, data => {
     $notification.post("配置刷新","成功","")
     $done({
-        title: "Profile Reload",
+        title: "𝑷𝒓𝒐𝒇𝒊𝒍𝒆 𝑹𝒆𝒍𝒐𝒂𝒅",
         content: "配置刷新成功",
         icon: "arrow.triangle.2.circlepath.doc.on.clipboard",
         "icon-color": "#8B81C3",
